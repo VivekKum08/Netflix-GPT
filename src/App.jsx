195b54a588a56d3874863161f7423b1526a1706a
Vivek Kumar <vivek08012003@gmail.com>
2024-07-11 14:04:18 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="text-3xl font-bold text-green-800">Vivek Kumar Rajput</div>
+    <div className="text-3xl font-bold text-green-800">Vivek Kumar Rajput from Goa</div>
   )
 }
 
